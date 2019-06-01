47094d3920a9b0d3d192b700adeebe6e00e46130
mhershko <hershko.michal@gmail.com>
2019-06-01 12:17:52 +0300

    added NER class, fixed errors

--- NER.py
+++ NER.py
@@ -1,19 +1,27 @@
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.tag import pos_tag
+from nltk.tag import StanfordNERTagger
 from nltk.chunk import conlltags2tree, tree2conlltags
 from pprint import pprint
 import spacy
+import os
 from spacy import displacy
 from collections import Counter
-#import en_core_web_sm
 
 
 class NER:
 
     def __init__(self):
+        java_path = "C:/Program Files/Java/jdk1.8.0_201/bin/java.exe"
+        os.environ['JAVAHOME'] = java_path
         nltk.download('punkt')
         nltk.download('averaged_perceptron_tagger')
+        self.stanford_ner_tagger = StanfordNERTagger(
+            'stanford-ner-2018-10-16/classifiers/english.all.3class.distsim.crf.ser.gz',
+            'stanford-ner-2018-10-16/stanford-ner-3.9.2.jar'
+        )
+        self.spacy_nlp = spacy.load('en_core_web_sm')
 
     # preprocess data using nltk library
     def preprocess_nltk(self, post):
@@ -34,6 +42,17 @@ class NER:
         ne_tree = nltk.ne_chunk(pos_tag(word_tokenize(post)))
         print(ne_tree)
 
+    def nltk_stanford_ner(self, post):
+        results = self.stanford_ner_tagger.tag(post.split())
+        print ("\nOriginal post: %s", post)
+        for result in results:
+            tag_value = result[0]
+            tag_type = result[1]
+            if tag_type != 'O':
+                print('Type: %s, Value: %s' % (tag_type, tag_value))
+
     def preprocess_spacy(self, post):
-        post = spacy.nlp(post)
-        pprint([(X.text, X.label_) for X in post.ents])
+        print("\nOriginal post: %s", post)
+        results = self.spacy_nlp(post)
+        for element in results.ents:
+            print('Type: %s, Value: %s' % (element.label_, element))
--- twitter_streaming.py
+++ twitter_streaming.py
@@ -1,5 +1,5 @@
 import tweepy
-import facebook
+from NER import NER
 
 #Variables that contains the user credentials to access Twitter API
 access_key = "1119680381479989248-H48YpATLs7DZvU9Zj7Xq0LS1hHh1QQ"
@@ -21,27 +21,29 @@ def get_tweets(username):
 
     # 200 tweets to be extracted
     number_of_tweets = 200
-    tweets = api.user_timeline(id="realDonaldTrump")
+    tweets = api.user_timeline(id=username)
 
     # Empty Array
-    tmp = []
+    tweets_list = []
 
     # create array of tweet information: username,
     # tweet id, date/time, text
     #tweets_for_csv = [tweet.text for tweet in tweets]  # CSV file created
     for tweet in tweets:
         # Appending tweets to the empty array tmp
-        tmp.append(tweet.text)
+        tweets_list.append(tweet.text)
 
-        # Printing the tweets
-    print(tmp)
+    # Printing the tweets
+    print(tweets_list)
+    return tweets_list
 
-
-def get_facebook_posts():
-    user_long_token = 'EAAg1mgNr5rMBABrx6sG1yTsFCvqv2rs5KgOyc10sDUmsKmTiu3b2RoJYea8hAJi6UZAmW0wymvdmFAY3IbBbP5IAeuxNrJm5SQccLuFgfVzwj35st11zNAiChNij5SVgTG854Vaypxr00KpRmNi6eRcOqqjQZD'
-    graph = facebook.GraphAPI(access_token=user_long_token, version="2.7")
 
 if __name__ == '__main__':
     # Here goes the twitter handle for the user
     # whose tweets are to be extracted.
-    get_tweets("twitter-handle")
+    tweets = get_tweets("realDonaldTrump")
+    ner = NER()
+    print("****** NLTK STANFORD NER ******")
+    l1 = [ner.nltk_stanford_ner(tweet) for tweet in tweets]
+    print("****** SPACY ******")
+    l2 = [ner.preprocess_spacy(tweet) for tweet in tweets]
